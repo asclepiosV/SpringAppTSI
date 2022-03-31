c3afd1772fbaa806ed716f14009d7bbb1e26bb69
Paul Flammarion <pa.flammarion@gmail.com>
2022-03-31 11:26:22 +0300

    Initial commit

---
+++ src/main/java/com/example/springapptsi/SpringAppTsiApplication.java
@@ -0,0 +1,13 @@
+package com.example.springapptsi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppTsiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppTsiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springapptsi/SpringAppTsiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springapptsi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppTsiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
